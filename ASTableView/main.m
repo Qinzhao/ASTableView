161b4d5d2cb0cd6ccd06a85437f106d2bf9fdb2c
AllenQin <601990575@qq.com>
2016-06-14 15:10:49 +0800

    提交ASTableView

---
+++ ASTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ASTableView
+//
+//  Created by AllenQin on 16/6/14.
+//  Copyright © 2016年 AllenQin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, "7.0"
+target :ASTableView do
+
+pod 'YYKit'
+pod 'Masonry'
+pod 'JPFPSStatus'
+pod 'SVProgressHUD'
+
+end
+
+
